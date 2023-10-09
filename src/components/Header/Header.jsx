db22af3f59db4180ba4dc61bea8b67bc36831e62
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2023-10-09 14:07:29 +0300

    feat: add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { IconLogo } from "./../icons/IconLogo";
+import { IconCart } from "./../icons/IconCart";
+
+import style from "./Header.module.scss";
+
+export const Header = () => {
+  return (
+    <header className={style.header}>
+      <div className={style.header__container}>
+        <div className={style.header__logo}>
+          <IconLogo />
+          <h1 className={style.header__title}>Tasty Pizza</h1>
+        </div>
+        <button className={style.header__btn}>
+          <span className={style.header__total}>0 ₽</span>
+          <span className={style.header__line}></span>
+          <span className={style.header__cart}>
+            <IconCart /> 0
+          </span>
+        </button>
+      </div>
+    </header>
+  );
+};
