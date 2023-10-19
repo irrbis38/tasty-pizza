5981d3d75f7d39e78060f202cd75b35850b3c307
Vasil Ilyuhin <v.ilyuhin@gmail.com>
2023-10-19 13:51:08 +0300

    feat: change header styles, add css-variables

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { IconLogoNew } from "./../icons/IconLogoNew";
+import { IconLogo } from "./../icons/IconLogo";
 import { IconCart } from "./../icons/IconCart";
 
 import style from "./Header.module.scss";
@@ -10,8 +10,8 @@ export const Header = () => {
     <header className={style.header}>
       <div className={style.header__container}>
         <div className={style.header__logo}>
-          <IconLogoNew />
-          {/* <h1 className={style.header__title}>Tasty Pizza</h1> */}
+          <IconLogo />
+          <h1 className={style.header__title}>Tasty Pizza</h1>
         </div>
         <button className={style.header__btn}>
           <span className={style.header__total}>0 ₽</span>
