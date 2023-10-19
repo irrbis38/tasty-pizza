fa605953b2769c09ff5f2fadfa36431890dabd10
Vasil Ilyuhin <v.ilyuhin@gmail.com>
2023-10-19 15:26:19 +0300

    feat: add panel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header/Header";
+import { Panel } from "./components/Panel/Panel";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Panel />
     </div>
   );
 }
---
+++ src/components/Panel/Panel.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import style from "./Panel.module.scss";
+
+export const Panel = () => {
+  return (
+    <nav className={style.panel}>
+      <div className="container">Panel</div>
+    </nav>
+  );
+};
