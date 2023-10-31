6886f1b88fc11dcf0643abb428bf75bf2d2ba500
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2023-10-31 13:56:33 +0300

    refactor: update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { IconLogo } from "./../icons/IconLogo";
+// import { IconLogo } from "./../icons/IconLogo";
 import { IconCart } from "./../icons/IconCart";
 
 import style from "./Header.module.scss";
@@ -10,8 +10,14 @@ export const Header = () => {
     <header className={style.header}>
       <div className={style.header__container}>
         <div className={style.header__logo}>
-          <IconLogo />
-          <h1 className={style.header__title}>Tasty Pizza</h1>
+          <div className={style.header__img}>
+            <img src="images/pizza-logo.png" alt="logo" />
+          </div>
+          <h1 className={style.header__title}>
+            <span>Tasty</span>
+            <br />
+            <span>Pizzeria Restaurant</span>
+          </h1>
         </div>
         <button className={style.header__btn}>
           <span className={style.header__total}>$ 0</span>
